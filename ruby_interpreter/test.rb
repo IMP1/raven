14e445a3ddca119f02117e06d4a4be820d93dbfa
IMP1 <huw_taylor@hotmail.co.uk>
2018-05-16 15:15:56 +0100

    tests count successes and failures

--- ruby_interpreter/test.rb
+++ ruby_interpreter/test.rb
@@ -1,30 +1,40 @@
 require_relative 'compiler'
 
 def test_file(filename, root="")
-    puts "running test '#{filename[root.length+1..-1]}':"
+    puts "running test '#{filename[root.length+1..-1]}'"
     success = true
     begin
         Compiler.run_file(filename)
-    rescue SystemExit => e
+    rescue SystemExit
         success = false
     end
-    puts "test #{success ? "succeeded" : "failed"}."
+    puts "test #{success ? "succeeded" : "failed"}"
+    puts
+    return success
     # TODO: make this nicer?
 end
 
 def test_folder(dirname, root=nil)
-    puts "running all tests in '#{dirname}'..."
+    successes = 0
+    failures = 0
+    puts "running all tests in '#{dirname}'"
     Dir[File.join(dirname, '*')].each do |f|
         if File.directory?(f)
-            test_folder(File.join(f), root || dirname)
+            s, f = test_folder(File.join(f), root || dirname)
+            successes += s
+            failures += f
         elsif File.file?(f)
-            test_file(File.join(f), root)
+            test_file(File.join(f), root) ? successes += 1 : failures += 1
         end
     end
+    return successes, failures
 end
 
 if ARGV.length == 1 && (ARGV[0] == "--all" || ARGV[0] == "-a")
-    test_folder(File.join(Dir.home, 'prog', 'raven', 'tests'))
+    s, f = test_folder(File.join(Dir.home, 'prog', 'raven', 'tests'))
+    puts "Ran #{s + f} tests"
+    puts "    #{s} succeeded"
+    puts "    #{f} failed"
 elsif ARGV.length > 0
     ARGV.each { |fn| test_file(fn) }
 end
