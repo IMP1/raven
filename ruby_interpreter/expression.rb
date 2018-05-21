508d1b95360c86dc5e712d6201344e3d54ecf3b4
Huw Taylor <huw_taylor@hotmail.co.uk>
2018-05-21 20:12:17 +0100

    gave all expressions types for upcoming type-checking and created function expression

--- ruby_interpreter/expression.rb
+++ ruby_interpreter/expression.rb
@@ -2,6 +2,13 @@ require_relative 'visitor'
 
 class Expression
     include Visitable
+
+    attr_reader :type
+
+    def initialize(type)
+        @type = type
+    end
+
 end
 
 class BinaryExpression < Expression
@@ -11,6 +18,7 @@ class BinaryExpression < Expression
     attr_reader :right
 
     def initialize(left, operator, right)
+        super(left.type)
         @left = left
         @operator = operator
         @right = right
@@ -24,6 +32,7 @@ class UnaryExpression < Expression
     attr_reader :right
 
     def initialize(operator, right)
+        super(right.type)
         @operator = operator
         @right = right
     end
@@ -35,6 +44,7 @@ class GroupingExpression < Expression
     attr_reader :expression
 
     def initialize(expr)
+        super(expr.type)
         @expression = expr
     end
 
@@ -43,11 +53,10 @@ end
 class LiteralExpression < Expression
 
     attr_reader :value
-    attr_reader :type
 
     def initialize(value, type)
+        super(type)
         @value = value
-        @type  = type
     end
 
 end
@@ -59,6 +68,7 @@ class ShortCircuitExpression < Expression
     attr_reader :right
 
     def intitialize(left, operator, right)
+        super(left.type)
         @left = left
         @operator = operator
         @right = right
@@ -71,7 +81,24 @@ class VariableExpression < Expression
     attr_reader :name
 
     def initialize(name)
+        super(nil)
         @name = name
+    end
+
+end
+
+class FunctionExpression < Expression
+
+    attr_reader :parameter_names
+    attr_reader :parameter_types
+    attr_reader :return_type
+    attr_reader :body
+
+    def initialize(parameters, return_type, body)
+        @parameter_names = parameters.map {|param| param[:name] }
+        @parameter_types = parameters.map {|param| param[:type] }
+        @return_type = return_type
+        @body = body
     end
 
 end
@@ -83,6 +110,7 @@ class CallExpression < Expression
     attr_reader :arguments
 
     def initialize(callee, token, arguments)
+        super(nil)
         @callee = callee
         @token = token
         @arguments = arguments
