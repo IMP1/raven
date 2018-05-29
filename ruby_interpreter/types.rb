49483170d8fba56f9fe1d2d8e8700ccfa5a6f22a
Huw Taylor <huw_taylor@hotmail.co.uk>
2018-05-29 20:27:21 +0100

    added some helper methods to types

--- ruby_interpreter/types.rb
+++ ruby_interpreter/types.rb
@@ -3,9 +3,15 @@ class Raven_Primitive
     def initialize(value)
         @value = value
     end
+    def to_s
+        return @value.to_s
+    end
 end
 
 class Raven_Integer < Raven_Primitive
+    def self.to_s
+        return "int"
+    end
 end
 
 class Raven_Real < Raven_Primitive
@@ -18,16 +24,30 @@ class Raven_Boolean < Raven_Primitive
 end
 
 class Raven_String < Raven_Primitive
+    def self.escape_string(str)
+        return str.gsub('\\n', "\n")
+    end
+
+    def initialize(value)
+        super(Raven_String.escape_string(value))
+    end
 end
 
 class Raven_Type < Raven_Primitive
+    def initialize(type_tree)
+        super(type_tree.size == 1 ? type_tree[0] : type_tree)
+    end
 end
 
 class Raven_Function
     attr_reader :parameters
-    attr_reader :return_value
-    def initialize(block, parameters, return_value)
-        
+    attr_reader :return_type
+    def initialize(block, parameters, return_type)
+        @block = block
+        @parameters = parameters
+        @return_type = return_type
+    end
+    def call(interpreter, args)
     end
 end
 
@@ -35,6 +55,12 @@ class Raven_Array
     attr_reader :elements
     def initialize(elements)
         @elements = elements
+    end
+    def self.to_s
+        return "array"
+    end
+    def to_s
+        return "[#{@elements.join(", ")}]"
     end
 end
 
