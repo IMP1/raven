53963ebec6a6331d18daf136221916cd223e0885
Huw Taylor <huw_taylor@hotmail.co.uk>
2018-06-03 17:57:14 +0100

    added scope debugging

--- ruby_interpreter/global_env.rb
+++ ruby_interpreter/global_env.rb
@@ -5,7 +5,7 @@ require_relative 'system_functions'
 class GlobalEnvironment < Environment
 
     def initialize
-        super
+        super("system")
         define(Token.system("p"), 
             lambda { |interpreter, args| p args[0] }, 
             [:func, [ [[:any]], [] ]])
@@ -15,6 +15,9 @@ class GlobalEnvironment < Environment
         define(Token.system("typeof"), 
             lambda { |interpreter, args| return SystemFunctions.type_of(args[0]) }, 
             [:func, [ [[:any]], [:type] ]])
+        define(Token.system("debug_scope"),
+            lambda { |interpreter, args| SystemFunctions.debug_scope(interpreter) },
+            [:func, [ [], [] ]])
     end
 
 end
