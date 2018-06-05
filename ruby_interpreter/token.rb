5f37f0b3350e305a7535d63db54f1c366bb496ed
IMP1 <huw_taylor@hotmail.co.uk>
2018-06-05 10:44:32 +0100

    addeded filename property

--- ruby_interpreter/token.rb
+++ ruby_interpreter/token.rb
@@ -4,14 +4,16 @@ class Token
     attr_reader :lexeme
     attr_reader :line
     attr_reader :column
+    attr_reader :filename
     attr_reader :literal
 
-    def initialize(name, lexeme, line, column, literal=nil)
-        @name    = name
-        @lexeme  = lexeme
-        @line    = line
-        @column  = column
-        @literal = literal
+    def initialize(name, lexeme, line, column, filename, literal=nil)
+        @name     = name
+        @lexeme   = lexeme
+        @line     = line
+        @column   = column
+        @filename = filename
+        @literal  = literal
     end
 
     def to_s
@@ -19,7 +21,7 @@ class Token
     end
 
     def self.system(lexeme)
-        return Token.new(:SYSTEM_FUNCTION, lexeme, 0, 0)
+        return Token.new(:SYSTEM_FUNCTION, lexeme, 0, 0, "Raven-Core")
     end
 
 end
